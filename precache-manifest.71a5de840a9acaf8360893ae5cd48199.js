7c7ebbd565516a4fb5063ccba91cf2ed4d1e6f5f
Johnny Interiano <benjonant@msn.com>
2019-09-15 02:40:03 -0400

    Updates

---
+++ precache-manifest.71a5de840a9acaf8360893ae5cd48199.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1a57f4367af8ddcf3c2f3dbb73c5c2",
+    "url": "/Suspect/index.html"
+  },
+  {
+    "revision": "a479cb870fb36a3b4369",
+    "url": "/Suspect/static/css/2.7ec30cc4.chunk.css"
+  },
+  {
+    "revision": "ccbfd97605c65b28efbe",
+    "url": "/Suspect/static/css/main.23242e25.chunk.css"
+  },
+  {
+    "revision": "a479cb870fb36a3b4369",
+    "url": "/Suspect/static/js/2.2b6dcaf0.chunk.js"
+  },
+  {
+    "revision": "ccbfd97605c65b28efbe",
+    "url": "/Suspect/static/js/main.53c07e41.chunk.js"
+  },
+  {
+    "revision": "90a07f571cd3352d0c95",
+    "url": "/Suspect/static/js/runtime~main.21446856.js"
+  },
+  {
+    "revision": "494bf87725b3b0879639aea0e5220ba4",
+    "url": "/Suspect/static/media/IG_Fill.494bf877.png"
+  }
+]);
